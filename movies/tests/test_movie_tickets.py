0da1cb626400b22df481ae56c74495000590079a
goutomroy <goutom.roy@easysolar-app.com>
2022-04-09 12:56:47 +0600

    Added test "test_non_owner_cant_get_others_single_ticket"

--- movies/tests/test_movie_tickets.py
+++ movies/tests/test_movie_tickets.py
@@ -9,7 +9,7 @@ from users.models import User
 
 class MovieTicketAPITestCase(APITestCase):
     def setUp(self) -> None:
-        self._movies_list_url = reverse("movies:movieticket-list")
+        self._movie_ticket_list_url = reverse("movies:movieticket-list")
         self._user = baker.make(User)
         self._client = APIClient()
         self._client.force_authenticate(self._user)
@@ -17,14 +17,14 @@ class MovieTicketAPITestCase(APITestCase):
     def test_purchase_ticket_successful(self):
         movie = baker.make(Movie)
         data = {"movie": movie.id, "user": self._user.id}
-        response = self._client.post(self._movies_list_url, data)
+        response = self._client.post(self._movie_ticket_list_url, data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_one_user_can_purchase_only_one_ticket_of_a_movie(self):
         movie = baker.make(Movie)
         baker.make(MovieTicket, movie=movie, user=self._user)
         data = {"movie": movie.id, "user": self._user.id}
-        response = self._client.post(self._movies_list_url, data)
+        response = self._client.post(self._movie_ticket_list_url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_all_tickets_of_a_movie_purchased_already(self):
@@ -34,7 +34,7 @@ class MovieTicketAPITestCase(APITestCase):
         another_user = baker.make(User)
         self._client.force_authenticate(another_user)
         data = {"movie": movie.id, "user": another_user.id}
-        response = self._client.post(self._movies_list_url, data)
+        response = self._client.post(self._movie_ticket_list_url, data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_get_list_of_purchased_tickets(self):
@@ -43,18 +43,18 @@ class MovieTicketAPITestCase(APITestCase):
         baker.make(MovieTicket, movie=movie_1, user=self._user)
         baker.make(MovieTicket, movie=movie_2, user=self._user)
 
-        response = self._client.get(self._movies_list_url)
+        response = self._client.get(self._movie_ticket_list_url)
         self.assertCountEqual(
             [movie_1.id, movie_2.id],
             [each["movie"] for each in response.data["results"]],
         )
 
-    def test_non_owner_cant_get_others_tickets(self):
+    def test_non_owner_cant_get_others_list_of_tickets(self):
         baker.make(MovieTicket, movie=baker.make(Movie), user=self._user)
         baker.make(MovieTicket, movie=baker.make(Movie), user=self._user)
 
         self._client.force_authenticate(baker.make(User))
-        response = self._client.get(self._movies_list_url)
+        response = self._client.get(self._movie_ticket_list_url)
         self.assertEqual(len([each["movie"] for each in response.data["results"]]), 0)
 
     def test_get_a_purchased_ticket(self):
@@ -64,3 +64,14 @@ class MovieTicketAPITestCase(APITestCase):
         )
         response = self._client.get(movie_detail_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_non_owner_cant_get_others_single_ticket(self):
+        baker.make(MovieTicket, movie=baker.make(Movie), user=self._user)
+        movie_ticket = baker.make(MovieTicket, movie=baker.make(Movie), user=self._user)
+
+        self._client.force_authenticate(baker.make(User))
+        movie_detail_url = reverse(
+            "movies:movieticket-detail", kwargs={"pk": movie_ticket.id}
+        )
+        response = self._client.get(movie_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
